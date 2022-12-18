4b18ada0c85e237c73bfd1561a229c69a295bd90
alok <alokkumar1997maurya@gmail.com>
2022-12-18 19:40:02 +0530

    fid two's power

--- JAVA Coding/BitManipulation/evenoddBit.java
+++ JAVA Coding/BitManipulation/evenoddBit.java
@@ -2,10 +2,12 @@
 class evenoddBit{
     public static void main(String[] args) {
         
-        int number = 6341;
+        int number = 634;
         if((number & 1) == 0)
             System.out.println("Even");
         else
         System.out.println("Odd");
+
+        // ((number & 1)==0)?System.out.println("Even"):System.out.println("odd");
     }
 }
---
+++ JAVA Coding/BitManipulation/findTwoOfPower.java
@@ -0,0 +1,10 @@
+public class findTwoOfPower {
+    public static void main(String[] args) {
+        int n =1 ;
+    if((n & n-1)==0){
+        System.out.println("yes");
+    }else{
+        System.out.println("NO");
+    }
+    }
+}
