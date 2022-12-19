0eb4ae6d7adc89aa3791bb865cf70e6cbc5130b0
alok <alokkumar1997maurya@gmail.com>
2022-12-19 12:30:19 +0530

    factorial code

---
+++ JAVA Coding/Recurtion/Factorial.java
@@ -0,0 +1,15 @@
+package Recurtion;
+
+public class Factorial {
+
+     public static int  FindFactorial(int number) {
+
+       if(number == 0)
+       return 1;
+       return number * FindFactorial(number-1);
+        
+     }
+    public static void main(String[] args) {
+        System.out.println(FindFactorial(6));
+    }
+}
