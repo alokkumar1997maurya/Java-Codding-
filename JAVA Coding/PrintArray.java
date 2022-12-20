cadd1bad1398b7cca0f77f7323263e0ed0b7b35a
alok <alokkumar1997maurya@gmail.com>
2022-12-20 14:51:00 +0530

    print Array Using Recursion

---
+++ JAVA Coding/PrintArray.java
@@ -0,0 +1,19 @@
+public class PrintArray {
+    public static void PrintArrayUsingRecursion(int[] arr, int idx) {
+        if(idx==arr.length){
+            return;
+        }
+        System.out.print(arr[idx] + " ");
+          PrintArrayUsingRecursion(arr, idx+1);
+        System.out.print(arr[idx] + " ");
+
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,6,4};
+        
+            PrintArrayUsingRecursion(arr, 0);
+        
+       
+    }
+}
